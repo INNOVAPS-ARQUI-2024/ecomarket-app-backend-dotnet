79ea32bf4d0313673be496ff6971ff0dac056dfa
Esteban Hernando Pedraza Solano <estemanps99@gmail.com>
2024-10-08 23:55:55 -0500

    Add files via upload

---
+++ scripts/deploy_docker_image-ecomarket-servicio-resenias-qa.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variables (puedes personalizarlas o recibirlas como parámetros)
+DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-resenias-qa.tar"
+DOCKER_IMAGE_NAME="ecomarket-servicio-resenias:qa"
+CONTAINER_NAME="ecomarket-servicio-resenias-qa"
+
+# Cargar la imagen Docker
+docker load -i $DOCKER_IMAGE_PATH
+
+# Detener y eliminar el contenedor anterior si existe
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+# Ejecutar un nuevo contenedor
+docker run -d --name $CONTAINER_NAME -p 8085:8080 $DOCKER_IMAGE_NAME
+
+# Opcional: eliminar el archivo .tar para ahorrar espacio
+rm $DOCKER_IMAGE_PATH
+
+echo "Deployment ecomarket-servicio-resenias-qa completed successfully!"
+
