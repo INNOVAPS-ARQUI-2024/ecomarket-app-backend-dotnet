c9706fdd1c08b064e0ca25afe73b37deba2d0365
Daniel Felipe Alzate <117049972+XxDaniel-AlzatexX@users.noreply.github.com>
2024-10-17 21:55:25 -0500

    Update deploy_docker_image-ecomarket-servicio-resenias-prod.sh

--- scripts/PROD/deploy_docker_image-ecomarket-servicio-resenias-prod.sh
+++ scripts/PROD/deploy_docker_image-ecomarket-servicio-resenias-prod.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Variables (puedes personalizarlas o recibirlas como parámetros)
-DOCKER_IMAGE_PATH="/tmp/ecomarket-servicio-resenias-prod.tar"
-DOCKER_IMAGE_NAME="ecomarket-servicio-resenias:prod"
-CONTAINER_NAME="ecomarket-servicio-resenias-prod"
-
-# Cargar la imagen Docker
-docker load -i $DOCKER_IMAGE_PATH
+# Variables
+DOCKER_IMAGE_NAME="devopsarqui2024/ecomarket-servicio-resenias:prod"
+CONTAINER_NAME="ecomarket-servicio-resenias"
+NETWORK_NAME="ecomarket-network"
 
 # Detener y eliminar el contenedor anterior si existe
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
+# Descargar la imagen desde Docker Hub
+docker pull $DOCKER_IMAGE_NAME
+
 # Ejecutar un nuevo contenedor
-docker run -d --name $CONTAINER_NAME -p 8085:8085 $DOCKER_IMAGE_NAME
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME -p 8085:8085 $DOCKER_IMAGE_NAME
 
-# Opcional: eliminar el archivo .tar para ahorrar espacio
-rm $DOCKER_IMAGE_PATH
-
-echo "Deployment ecomarket-servicio-resenias-prod completed successfully!"
-
+echo "Deployment ecomarket-servicio-resenias-qa completed successfully!"
